74389dddaaa5150e20f4ef4e8a689e3649751fdf
Barbosa Thaíssa <thathamaria03@gmail.com>
2023-10-14 20:29:23 -0300

    feat: organização

---
+++ SaidaDeDados/SaidaDeDados/Loops.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaidaDeDados
+{
+    internal class Loops
+    {
+        public void MetodosLoops()
+        {
+            Console.WriteLine("COMEÇO LOOPS");
+
+
+            Console.WriteLine("FIM");
+        }
+    }
+}
